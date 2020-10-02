a12b1ab2a460c179e531786718c8cbd8cab0d700
Alex <Alex@im-business.com>
2020-10-02 21:05:04 +0700

    FIX calculate total delivery

--- services/DeliveryService.php
+++ services/DeliveryService.php
@@ -159,7 +159,7 @@ class DeliveryService extends Component
 
     private function getCalculateTotal(Setting $setting, $total)
     {
-        if ($setting->cost_delivery && !$setting->markup) return $cost_delivery;
+        if ($setting->cost_delivery && !$setting->markup) return $setting->cost_delivery;
         elseif ($setting->markup && !$setting->cost_delivery) return $total + $total * $setting->markup / 100;
         elseif($setting->markup && $setting->cost_delivery) return $setting->cost_delivery + $setting->cost_delivery * $setting->markup / 100;
         else return $total;
