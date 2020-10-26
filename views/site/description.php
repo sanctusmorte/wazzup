013c606883cf8278dcae942d7cc7b7f9eca770c7
Игорь Зайцев <igor@im-business.com>
2020-10-26 14:49:03 +0300

    description.php edited online with Bitbucket

--- views/site/description.php
+++ views/site/description.php
@@ -15,32 +15,32 @@ $this->title = 'Интеграция Logsis и retailCRM';
 <p>Модуль интеграции Logsis с retailCRM.</p>
 <h3>Подробная инструкция по установке и активации</h3>
 <ul>
-<li>Иметь свой личный кабинет в <a href="http://cab.logsis.ru" target="_blank" rel="noopener">Logsis</a>.</li>
+<li>1.Иметь свой личный кабинет в <a href="http://cab.logsis.ru" target="_blank" rel="noopener">Logsis</a>.</li>
 <li>
-<p>Создать свой API ключ Logsis. Для этого перейти по <a href="http://cab.logsis.ru/edit-profile-dash" target="_blank" rel="noopener">ссылке</a>, потом перейти на вкладку Профиль</p>
+<p>2.Создать свой API ключ Logsis. Для этого перейти по <a href="http://cab.logsis.ru/edit-profile-dash" target="_blank" rel="noopener">ссылке</a>, потом перейти на вкладку Профиль</p>
 <img src="/img/image5.png" alt="" width="100%" />
 <p>Скопировать API ключ</p>
 <img src="/img/image6.png" alt="" width="100%" /></li>
 <li>
-<p>Потом перейти в retailCRM модуль Logsis там начать на кнопку &ldquo;перейти в личный кабинет&rdquo;.</p>
+<p>3.Потом перейти в retailCRM модуль Logsis там начать на кнопку &ldquo;перейти в личный кабинет&rdquo;.</p>
 <img src="/img/image8.png" alt="" width="100%" /></li>
 <li>
-<p>Сгенерировать API-ключ в retailCRM для этого перейти в раздел Администрирование &gt; Интеграция &gt; Ключи доступа к API и создать API-ключ. (в создаваемом API-ключе, в поле &laquo;Разрешенные методы API&raquo;, должны быть активированы методы &laquo;Доставки&raquo; и &laquo;Интеграция&raquo;.) <br /><strong>Интеграция</strong><br />/api/integration-modules/{code}<br />/api/integration-modules/{code}/edit <br /><br /><strong>Доставки</strong><br />/api/delivery/generic/setting/{code}<br />/api/delivery/generic/setting/{code}/edit<br />/api/delivery/generic/{code}/tracking<br />/api/delivery/shipments<br />/api/delivery/shipments/{id}<br />/api/delivery/shipments/create<br />/api/delivery/shipments/{id}/edit </p>
+<p>4.Сгенерировать API-ключ в retailCRM для этого перейти в раздел Администрирование &gt; Интеграция &gt; Ключи доступа к API и создать API-ключ. (в создаваемом API-ключе, в поле &laquo;Разрешенные методы API&raquo;, должны быть активированы методы &laquo;Доставки&raquo; и &laquo;Интеграция&raquo;.) <br /><strong>Интеграция</strong><br />/api/integration-modules/{code}<br />/api/integration-modules/{code}/edit <br /><br /><strong>Доставки</strong><br />/api/delivery/generic/setting/{code}<br />/api/delivery/generic/setting/{code}/edit<br />/api/delivery/generic/{code}/tracking<br />/api/delivery/shipments<br />/api/delivery/shipments/{id}<br />/api/delivery/shipments/create<br />/api/delivery/shipments/{id}/edit</p>
 <br />
 <p>После нажатия открываются настройки доставки, где необходимо ввести раннее созданный API-ключ, а также адрес Вашей системы и нажать на кнопку &laquo;Сохранить&raquo;.</p>
 </li>
 <li>
-<p>В личном кабинете Logsis ввести ссылку на ваш аккаунт retailCRM, API ключ retailCRM и API ключ Logsis.</p>
+<p>5.В личном кабинете Logsis ввести ссылку на ваш аккаунт retailCRM, API ключ retailCRM и API ключ Logsis.</p>
 <img src="/img/image2.png" alt="" width="100%" />
-<p>Во вкладке &ldquo;Магазин&rdquo; ввести магазин который вы ввели в 2 пункте.</p>
+<p>Во вкладке &ldquo;Магазин&rdquo; ввести магазин который вы ввели в 4 пункте.</p>
 <img src="/img/image3.png" alt="" width="100%" /></li>
 <li>
-<p>Во вкладке &ldquo;общие настройки&rdquo; установить нужные вам настройки.</p>
+<p>6.Во вкладке &ldquo;общие настройки&rdquo; установить нужные вам настройки.</p>
 <img src="/img/image7.png" alt="" width="100%" /></li>
 <li>
 <p>Во вкладке &ldquo;настройках статусов&rdquo; настроить соотношение статусов в logsis и retailCRM.</p>
 <img src="/img/image4.png" alt="" width="100%" /></li>
-<li>Для проверки работы модуля мы создаем заказ в retailCRM выбираем тип доставки Logsis, обязательно при этом выбрать магазин, тот который был указан в API КЛЮЧЕ из 4 пункта, после этого нажимаем на кнопочку &ldquo;выбрать тариф&rdquo;, после выбора тарифа, сохраняем заказ и проверяем отобразился ли он в вашем личном кабинете в <a href="http://cab.logsis.ru/orders2/olist" target="_blank" rel="noopener">Logsis.</a></li>
+<li>7. Для проверки работы модуля мы создаем заказ в retailCRM выбираем тип доставки Logsis, обязательно при этом выбрать магазин, тот который был указан в API КЛЮЧЕ из 4 пункта, после этого нажимаем на кнопочку &ldquo;выбрать тариф&rdquo;, после выбора тарифа, сохраняем заказ и проверяем отобразился ли он в вашем личном кабинете в <a href="http://cab.logsis.ru/orders2/olist" target="_blank" rel="noopener">Logsis.</a></li>
 </ul>
 <p>О поддержке модуля Вы можете узнать в разделе <code>Поддержка</code> (<!--?= Html::a('перейти', ['/site/support']) ?-->).</p>
 </div>
