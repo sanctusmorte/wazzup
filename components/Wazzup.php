6be6c82176737b56eb4f3f9b45a1ef1e7ccc4d8c
User <maksmoney87@gmail.com>
2021-02-17 20:47:33 +0500

    test

--- components/Wazzup.php
+++ components/Wazzup.php
@@ -97,19 +97,16 @@ class Wazzup extends Component
         $url = 'https://api.wazzup24.com/v2/channels';
 
         $response = json_decode($this->makeGetRequest($url, $apiKey), 1);
-
-        var_dump($response);
-
-//        if (isset($response['error'])) {
-//            return [
-//                'success' => false,
-//                'errorMsg' => $response['error']['code']
-//            ];
-//        } else {
-//            return [
-//                'success' => true,
-//                'errorMsg' => $response['error']['code']
-//            ];
-//        }
+        
+        if (isset($response['error'])) {
+            return [
+                'success' => false,
+                'errorMsg' => $response['error']['code']
+            ];
+        } else {
+            return [
+                'success' => true,
+            ];
+        }
     }
 }
