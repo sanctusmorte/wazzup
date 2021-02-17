14684791a888813e053ee8ff3edefabe9630c434
User <maksmoney87@gmail.com>
2021-02-17 21:13:41 +0500

    test

--- services/WazzupService.php
+++ services/WazzupService.php
@@ -29,15 +29,15 @@ class WazzupService
 
         if ($setting !== null) {
             $needChanneId = null;
-            $existChannels = $setting->wazzup_channels;
-            Yii::error($existChannels, 'wazzup_telegram_log');
+            $existChannels = json_decode($setting->wazzup_channels, 1);
+
             foreach ($existChannels as $existChannel) {
                 if ($existChannel['externalId'] === $needChannelExternalId) {
                     $needChanneId = $existChannel['id'];
                     break;
                 }
             }
-            
+            Yii::error($needChanneId, 'wazzup_telegram_log');
             if ($needChanneId !== null) {
 
                 Yii::$app->transport->sentMessageToRetailCrm($setting, $message, $needChanneId);
