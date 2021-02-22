0f6883052da8c9a789a3b486b3575af39721cb32
User <maksmoney87@gmail.com>
2021-02-22 21:01:22 +0500

    test

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -48,22 +48,6 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        var_dump(phpinfo());
-
-//        $client = new Client('https://mg-s1.retailcrm.pro', '2844d55a3b95d43ebdef57e219207f22b029b5425c53214f1c19d24d72bfe19cba88', true);
-//
-//        try {
-//            $request = new DialogAssignRequest();
-//            $request->setDialogId(999);
-//            $request->setUserId(1);
-//
-//            /* @var \RetailCrm\Mg\Bot\Model\Response\AssignResponse $response */
-//            $response = $client->dialogAssign($request);
-//            var_dump($response);
-//        } catch (Exception\LimitException | Exception\InvalidJsonException | Exception\UnauthorizedException $exception) {
-//            echo $exception->getMessage();
-//        }
-
         return $this->render('index');
     }
 
