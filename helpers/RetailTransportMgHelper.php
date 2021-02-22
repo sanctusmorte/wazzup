7edcd5f965afec4c8bec91ed88f281f039bfabf9
User <maksmoney87@gmail.com>
2021-02-22 19:36:39 +0500

    test

--- helpers/RetailTransportMgHelper.php
+++ helpers/RetailTransportMgHelper.php
@@ -41,7 +41,7 @@ class RetailTransportMgHelper
 
 
         if (isset($message['content'])) {
-            $uploadFile = Yii::$app->transpot->uploadFileByUrl($data, ['url' => $message['content']]);
+            $uploadFile = Yii::$app->transport->uploadFileByUrl($data, ['url' => $message['content']]);
             Yii::error($uploadFile, 'wazzup_telegram_log');
 //            if (isset($uploadFile['id'])) {
 //                $body['Message']['Type'] = 'image';
