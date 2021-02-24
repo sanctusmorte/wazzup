78f23bccb2488660ab5bfe1e589d6ab4ed72ba41
User <maksmoney87@gmail.com>
2021-02-24 15:29:59 +0500

    test setting

--- controllers/WazzupController.php
+++ controllers/WazzupController.php
@@ -47,7 +47,7 @@ class WazzupController extends Controller
                 } else {
                     $message = json_decode($data, 1);
 
-
+                    Yii::error($message, 'wazzup_telegram_log');
 
                     if (isset($message['messages'])) {
                         $this->wazzupService->handleMessageFromWazzup($message['messages'], $existSetting);
