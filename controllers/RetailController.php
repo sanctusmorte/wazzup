41e90a6a244d614c30577d43e3c654b0f0767802
User <maksmoney87@gmail.com>
2021-03-11 19:07:54 +0500

    test

--- controllers/RetailController.php
+++ controllers/RetailController.php
@@ -45,6 +45,11 @@ class RetailController extends Controller
                 } else {
                     $message = json_decode($data, 1);
 
+                    if ($existSetting->wazzup_web_hook_uuid === 'FuzoV68F4Caqolhsoqh8AmoWXaV1A4YV') {
+                        Yii::error($message, 'wazzup_telegram_log');
+                    }
+
+
                     if (isset($message['type'])) {
                         $this->retailTransportMgService->handleMessageFromRetail($message, $existSetting);
                         $response = [
