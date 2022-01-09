76b8bdd5b9c90ea852085be5e80dafeb23fc09b3
Maksim Zlivko <maksmoney87@gmail.com>
2022-01-09 23:16:07 +0500

    fix

--- controllers/RetailController.php
+++ controllers/RetailController.php
@@ -46,6 +46,8 @@ class RetailController extends Controller
 
                 $data = file_get_contents('php://input');
 
+                Yii::error(json_encode($data), 'wazzup_telegram_log');
+
                 if ($data === null) {
                     return http_response_code(200);
                 } else {
